75520dbc1ff91b38a8c28b0cac448158b30d0230
rizqyhbb <rizqyhbb@gmail.com>
2022-02-10 12:40:19 +0700

    add task done

--- src/controllers/query-controller.js
+++ src/controllers/query-controller.js
@@ -3,7 +3,8 @@ const QueryController = {
   getUserById: "SELECT id, username, email FROM users WHERE id = $1",
   getUserByEmail: "SELECT * from users WHERE email = $1",
   addUser: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
-  getTaskByFk: "SELECT * FROM todos WHERE user_id = $1"
+  getTaskByFk: "SELECT * FROM todos WHERE user_id = $1",
+  addTask: "INSERT INTO todos (task, user_id) VALUES ($1, $2)"
 }
 
 module.exports = QueryController;
--- src/services/task-service.js
+++ src/services/task-service.js
@@ -1,5 +1,6 @@
 const pool = require('../config/db');
-const { getTaskByFk } = require('../controllers/query-controller');
+const { getTaskByFk, addTask } = require('../controllers/query-controller');
+const { isEmpty } = require('../utils/validator')
 
 class TaskService {
   static getAllTask = async (id) => {
@@ -16,7 +17,8 @@ class TaskService {
       if (isEmpty(task)) {
         throw new Error(ERRORS.INPUT_CANNOT_BE_EMPTY);
       }
-
+      await pool.query(addTask, [task, id])
+      return
     } catch (err) {
       throw err
 
