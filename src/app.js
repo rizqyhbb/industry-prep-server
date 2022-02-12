fa9426df7b03d349fe7f35f7b65e2aa1e024b73f
rizqyhbb <rizqyhbb@gmail.com>
2022-02-12 21:06:23 +0700

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
+const cors = require('cors');
 const express = require('express');
-const apiRoutes = require('./routes/index')
+const apiRoutes = require('./routes/index');
 
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/v1', apiRoutes)
 
