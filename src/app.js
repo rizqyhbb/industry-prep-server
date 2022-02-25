4e72edaa5e13b932a04e780ad1bb4e743fe95c1b
rizqyhbb <rizqyhbb@gmail.com>
2022-02-26 05:50:08 +0700

    adding new cors config

--- src/app.js
+++ src/app.js
@@ -7,20 +7,28 @@ const app = express();
 const PORT = process.env.PORT
 
 // app.use(cors())
-let whiteList = ['http://localhost:3000']
-let corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (whiteList.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }
-  } else {
-    corsOptions = { origin: false }
-  }
-  callback(null, corsOptions)
+// let whiteList = ['http://localhost:3000']
+// let corsOptionsDelegate = function (req, callback) {
+//   let corsOptions;
+//   if (whiteList.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true }
+//   } else {
+//     corsOptions = { origin: false }
+//   }
+//   callback(null, corsOptions)
+// }
+
+const corsOption = {
+  "origin": "http://localhost:3000",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
 }
 
 
+
 app.use(express.json())
-app.use('/api/v1', cors(corsOptionsDelegate), apiRoutes)
+app.use('/api/v1', cors(corsOption), apiRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server runninh on port ${PORT}`)
