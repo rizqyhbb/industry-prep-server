59aa9f6557557445272f203fa8554144bb4250cf
M. Rizqy Fadhilah <582898@gmf-aeroasia.co.id>
2022-02-25 14:35:06 +0700

    adding cors whitelist

--- src/app.js
+++ src/app.js
@@ -6,9 +6,20 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT
 
-app.use(cors())
+// app.use(cors())
+let whiteList = ['http://localhost:3000']
+let corsOption = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }  
+}
+
 app.use(express.json())
-app.use('/api/v1', apiRoutes)
+app.use('/api/v1', cors(corsOption),apiRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server runninh on port ${PORT}`)
