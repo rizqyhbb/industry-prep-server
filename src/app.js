e8dc86aff10a36f96203b91d3506dd50de728f6d
rizqyhbb <rizqyhbb@gmail.com>
2022-02-26 06:49:04 +0700

    cors change

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 
-app.use(cors())
+// app.use(cors())
 // let whiteList = ['http://localhost:3000']
 // let corsOptionsDelegate = function (req, callback) {
 //   let corsOptions;
@@ -20,6 +20,14 @@ app.use(cors())
 //   callback(null, corsOptions)
 // }
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*")
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
+app.get()
 
 app.use('/api/v1', apiRoutes)
 
