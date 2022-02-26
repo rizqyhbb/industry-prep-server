fb65ba5684edab910245c51ed69a4b3fa2fdc492
rizqyhbb <rizqyhbb@gmail.com>
2022-02-26 10:15:18 +0700

    re set cors to its original

--- src/app.js
+++ src/app.js
@@ -8,26 +8,7 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 
-// app.use(cors())
-// let whiteList = ['http://localhost:3000']
-// let corsOptionsDelegate = function (req, callback) {
-//   let corsOptions;
-//   if (whiteList.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true }
-//   } else {
-//     corsOptions = { origin: false }
-//   }
-//   callback(null, corsOptions)
-// }
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*")
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-})
-
-app.get()
+app.use(cors())
 
 app.use('/api/v1', apiRoutes)
 
--- src/middlewares/cors.js
+++
@@ -1,8 +0,0 @@
-const access = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*")
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-}
-
-module.exports = access;
